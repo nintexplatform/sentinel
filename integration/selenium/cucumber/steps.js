97f36aba23b26af65642e676ff68355151dfceb9
Peter De Vries <peter.devries@nintex.com>
2018-01-18 16:51:21 +1100

    feat(selenium): Ensure that the selenium driver is built on demand. (#15)
    Wait for selenium service to response successfully prior to configuring a driver.
    Tests that do not use selenium will no longer fail due to selenium issues when running too soon

--- integration/selenium/cucumber/steps.js
+++ integration/selenium/cucumber/steps.js
@@ -2,10 +2,10 @@ module.exports = function () {
   const autUrl = process.env.AUT_SERVER_URL || '.';
 
   this.Given(/^I have loaded the web application$/, { timeout: 10000 }, async function () {
-    return await this.driver.get(`${autUrl}`);
+    return this.driver.get(`${autUrl}`);
   });
 
   this.Given(/^I have loaded the web application with "([^"]+)"$/, { timeout: 10000 }, async function (url) {
-    return await this.driver.get(url);
+    return this.driver.get(url);
   });
 };
