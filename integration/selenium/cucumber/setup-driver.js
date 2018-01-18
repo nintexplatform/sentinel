f80915bd03460747b23b5c7dbb5dd5952b88a857
Peter De Vries <peter.devries@nintex.com>
2018-01-18 15:13:35 +1100

    feat(selenium): Ensure that the selenium driver is built on demand. Wait for selenium service to response successfully prior to configuring a driver. Tests that do not use selenium will no longer fail due to selenium issues when running too soon

--- integration/selenium/cucumber/setup-driver.js
+++ integration/selenium/cucumber/setup-driver.js
@@ -6,8 +6,10 @@
 
 const webdriver = require('selenium-webdriver');
 const proxy = require('selenium-webdriver/proxy');
+const chrome = require('selenium-webdriver/chrome');
 const fs = require('fs');
 const { URL } = require('url');
+const rp = require('request-promise-native');
 
 function parseTimeout(val, ifNanValue) {
   const parsed = parseInt(val, 10);
@@ -24,59 +26,119 @@ const env = {
   proxyURL: process.env.ZAP_SERVER_URL || 'http://zap:8080',
   pageLoadTimeout: parseTimeout(process.env.WEBDRIVER_PAGE_TIMEOUT, 45000),
   remoteSeleniumcapabilities: process.env.SELENIUM_REMOTE_CAPABILITY,
+  waitForSeleniumTimeout: parseTimeout(process.env.SELENIUM_SERVICE_START_TIMEOUT, 45000),
 };
 
-function configureDriver() {
-  const proxyTestcapabilities = {
-    browserName: `${env.browser}`,
-    chromeOptions: {
-      excludeSwitches: ['ignore-certificate-errors'],
-    },
-  };
+async function waitForSeleniumDriver() {
+  let resp;
+  let timedOut = false;
+  const timeout = Date.now() + env.waitForSeleniumTimeout;
+  const url = new URL(env.remoteUrl);
+  do {
+    try {
+      resp = await rp({ // eslint-disable-line no-await-in-loop
+        uri: url.href,
+        resolveWithFullResponse: true,
+      });
+    } catch (e) {
+      // Ignore connection errors because we are waiting for the container to start
+      resp = { statusCode: 418 };
+      await new Promise(r => setTimeout(r, 10000)); // eslint-disable-line no-await-in-loop
+    }
+    timedOut = Date.now() > timeout;
+    if (timedOut) {
+      throw new Error('Timeout waiting for request to succeed');
+    }
+  } while (resp.statusCode !== 200);
+}
 
-  // Initate WebDriver Object based on Environment
-  if (env.executionEnv === 'local') {
-    this.driver = new webdriver.Builder()
-      .forBrowser(env.browser)
-      .build();
-    this.driver.manage().window().setSize(1920, 1080);
-  } else if (env.executionEnv === 'proxy') {
+function createDriver() {
+  let builder = new webdriver.Builder()
+    .forBrowser(env.browser);
+
+  if (env.executionEnv === 'proxy') {
     const url = new URL(env.proxyURL);
-    this.driver = new webdriver.Builder()
-      .forBrowser(env.browser)
-      .withCapabilities(proxyTestcapabilities)
-      .setProxy(proxy.manual({ http: `${url.host}` }))
-      .build();
-    this.driver.manage().window().setSize(1920, 1080);
-  } else if (env.executionEnv === 'remote') {
+    const options = new chrome.Options();
+    builder = builder
+      .withCapabilities(options.toCapabilities())
+      .setProxy(proxy.manual({ http: `${url.host}` }));
+  }
+
+  if (env.executionEnv === 'remote') {
     let remoteSeleniumcapabilitiesParams;
     if (env.remoteSeleniumcapabilities) {
       remoteSeleniumcapabilitiesParams = JSON.parse(fs.readFileSync(env.remoteSeleniumcapabilities, 'utf8'));
     }
-    this.driver = new webdriver.Builder()
-      .usingServer(`${env.remoteUrl}`)
-      .withCapabilities(remoteSeleniumcapabilitiesParams)
-      .build();
-    this.driver.manage().window().maximize();
+    builder = builder.withCapabilities(remoteSeleniumcapabilitiesParams);
   }
+
+  const driver = builder.build();
+
+
+  if (env.executionEnv !== 'remote') {
+    driver.manage().window().setSize(1920, 1080);
+  } else {
+    driver.manage().window().maximize();
+  }
+  driver.manage().timeouts().pageLoadTimeout(env.pageLoadTimeout);
+  return driver;
+}
+
+function configureDriver() {
+  let driver;
+  let hasDriver = false;
+  Object.defineProperty(this, 'hasDriver', {
+    get: () => hasDriver,
+    set: (value) => {
+      if (hasDriver && !value) {
+        driver = undefined;
+      }
+      hasDriver = value;
+    },
+    configurable: true,
+    enumerable: true,
+  });
+  Object.defineProperty(this, 'driver', {
+    get: () => {
+      if (!hasDriver) {
+        driver = createDriver();
+        hasDriver = !!driver;
+      }
+      return driver;
+    },
+    configurable: true,
+    enumerable: true,
+  });
   this.getDriver = () => this.driver;
-
-  return this.driver.manage().timeouts().pageLoadTimeout(env.pageLoadTimeout);
 }
 
-function stopDriver() {
-  return this.driver.quit();
-}
-
-function saveScreenshot(scenario) {
-  if (!scenario.isFailed()) {
-    return Promise.resolve();
+async function stopDriver() {
+  if (this.hasDriver) {
+    await this.driver.quit();
   }
-  return this.driver.takeScreenshot()
-    .then(data => scenario.attach(Buffer.from(data, 'base64'), 'image/png'));
+  this.hasDriver = false;
+}
+
+async function saveScreenshot(scenario) {
+  if (!this.hasDriver) {
+    return;
+  }
+  if (!scenario.isFailed()) {
+    return;
+  }
+  // On a failed scenario, take a screenshot
+  try {
+    const data = await this.driver.takeScreenshot();
+    scenario.attach(Buffer.from(data, 'base64'), 'image/png');
+  } catch (e) {
+    // Do not report another error here if the driver is unavailable.
+  }
 }
 
 module.exports = function () {
+  this.registerHandler('BeforeFeatures', {
+    timeout: env.waitForSeleniumTimeout,
+  }, waitForSeleniumDriver);
   this.Before(configureDriver);
   this.After(stopDriver);
   this.After(saveScreenshot);
