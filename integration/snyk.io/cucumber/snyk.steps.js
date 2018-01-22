8c85aca89af43852a85390e14831ef3874a9f809
Peter De Vries <peter.devries@nintex.com>
2018-01-22 11:21:09 +1100

    feat(snyk.io): Only fail when severity is above configured threshold. (#16)
    Attach vulnerability info to the cucumber report.

--- integration/snyk.io/cucumber/snyk.steps.js
+++ integration/snyk.io/cucumber/snyk.steps.js
@@ -1,28 +1,27 @@
-/* eslint
-   new-cap: 0,
-   func-names: 0,
-   prefer-arrow-callback: 0,
-   no-console: 0,
-*/
+const assert = require('assert');
 const env = require('./environment');
 const Snyk = require('../pages/snyk.io');
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-
-chai.use(chaiAsPromised);
-const assert = chai.assert;
-const errorMessage = 'Vulnerability detected';
-let snykOutput = '';
 
 module.exports = function () {
+  this.Before(function () {
+    this.snykOutput = null;
+  });
+
   this.When(/^the SNYK command is run against the project dependencies$/, { timeout: env.longTimeout }, async function () {
     const snyk = new Snyk();
-    snykOutput = await snyk.startTheProcess();
-    console.log(snykOutput);
+    this.snykOutput = await snyk.startTheProcess();
   });
 
   this.When(/^there should not be any vulnerable paths found$/, { timeout: env.longTimeout }, async function () {
-    const expectedValue = 'no vulnerable paths found';
-    assert((snykOutput.indexOf(expectedValue) !== -1), `${errorMessage}`);
+    if (this.snykOutput) {
+      assert(this.snykOutput.every(v =>
+        v.severity && v.severity.toLowerCase() !== env.snykFailureLevel.toLowerCase()), 'Vulnerability detected');
+    }
+  });
+
+  this.After(async function (scenario) {
+    if (this.snykOutput) {
+      scenario.attach(JSON.stringify(this.snykOutput, null, 2));
+    }
   });
 };
--- integration/snyk.io/service/index.js
+++ integration/snyk.io/service/index.js
@@ -9,13 +9,60 @@ const router = require('koa-router')({
 
 const port = process.env.SVC_PORT || '8086';
 
+function groupVulnerabilityLines(lines) {
+  let groupOfLines = [];
+  const hasLines = () => groupOfLines.length > 0;
+  const isNewGroup = line => line && line.includes('severity vulnerability found');
+  const hasFinished = line => line && line.includes('dependencies for known vulnerabilities');
+  return lines.reduce((agg, line) => {
+    if (hasLines() && (isNewGroup(line) || hasFinished(line))) {
+      agg.push(groupOfLines);
+      groupOfLines = [];
+    }
+    if (isNewGroup(line) || hasLines()) {
+      groupOfLines.push(line);
+    }
+    return agg;
+  }, []);
+}
+
+function parseGroupOfLines(group) {
+  const typeReg = /(\w+) severity vulnerability found on (.+)$/;
+  const descReg = /desc: (.+)$/;
+  const infoReg = /info: (.+)$/;
+  const fromReg = /from: (.+)$/;
+  return group.reduce((result, line) => {
+    /* eslint-disable no-param-reassign */
+    let matches = typeReg.exec(line);
+    if (matches) {
+      [, result.severity, result.package] = matches;
+    }
+    matches = descReg.exec(line);
+    if (matches) {
+      [, result.desc] = matches;
+    }
+    matches = infoReg.exec(line);
+    if (matches) {
+      [, result.info] = matches;
+    }
+    matches = fromReg.exec(line);
+    if (matches) {
+      [, result.from] = matches;
+    }
+    /* eslint-enable no-param-reassign */
+    return result;
+  }, {});
+}
+
+function parseResult(result) {
+  const lines = result.split('\n');
+  const vulnerabilities = groupVulnerabilityLines(lines);
+  return vulnerabilities.map(parseGroupOfLines);
+}
+
 router.get('/snyk/run', async (ctx) => {
-  try {
-    const result = await exec('sh ./snyk.sh');
-    ctx.body = result.stdout;
-  } catch (err) {
-    ctx.body = err;
-  }
+  const result = await exec('sh ./snyk.sh');
+  ctx.body = parseResult(result.stdout);
 });
 
 app
