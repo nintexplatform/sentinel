66a22445eac5bf55b1483857324e9d27196c0d87
Peter De Vries <peter.devries@nintex.com>
2018-03-22 17:52:30 +1100

    feat(snyk): Remove the child-process-promise package. Allow scanning in a subdirectory

--- integration/snyk.io/pages/snyk.io.js
+++ integration/snyk.io/pages/snyk.io.js
@@ -2,10 +2,21 @@ const rp = require('request-promise');
 const env = require('./../cucumber/environment');
 
 class Snyk {
-  async startTheProcess() {
+  async runScanInProjectDirectory() {
     this.output = await rp({
       uri: `${env.snykUrl}snyk/run`,
       json: true,
+    });
+    return this.output;
+  }
+  async runScanInDirectory(dir) {
+    this.output = await rp({
+      uri: `${env.snykUrl}snyk/run`,
+      method: 'POST',
+      json: true,
+      body: {
+        working_directory: dir,
+      },
     });
     return this.output;
   }
