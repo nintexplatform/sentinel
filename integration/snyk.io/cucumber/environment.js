21e05ddf9ec35259f9a28dc95e55b282a41e5810
Peter De Vries <peter.devries@nintex.com>
2019-05-06 18:16:22 +1000

    Fix/snyk failure level (#26)
    * fix(snyk): Updates the severity threshold to fail when configured for medium and a high severity issue is identified
    
    * fix(node_modules): Update packages with security fixes
    
    * feat(version): Bumps the package version

--- integration/snyk.io/cucumber/environment.js
+++ integration/snyk.io/cucumber/environment.js
@@ -1,6 +1,13 @@
 const sharedEnv = require('../../../lib/environment');
 
+const configuredThreshold = (process.env.SNYK_FAILURE_LEVEL || 'high').toLowerCase();
+const possibleThresholds = ['low', 'medium', 'high'];
+
+if (!possibleThresholds.includes(configuredThreshold)) {
+  throw new Error(`Invalid snyk severity threshold: ${configuredThreshold}`);
+}
+
 module.exports = Object.assign({}, sharedEnv, {
   snykUrl: process.env.SNYK_URL || 'http://snyk:8086/',
-  snykFailureLevel: process.env.SNYK_FAILURE_LEVEL || 'high',
+  snykFailureLevels: possibleThresholds.slice(possibleThresholds.indexOf(configuredThreshold)),
 });
