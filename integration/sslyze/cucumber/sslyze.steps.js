96693c1c039e7ffbccbcbebed0faa53bd36fb25f
Peter De Vries <peter.devries@nintex.com>
2020-06-18 19:53:23 +1000

    feat(sslyze): Updates to use json output to find supported ciphers. (#43)
    Makes parsing easier and less error prone.
    Adds scan result caching to prevent unnecessary repeated scans on the same host

--- integration/sslyze/cucumber/sslyze.steps.js
+++ integration/sslyze/cucumber/sslyze.steps.js
@@ -14,30 +14,54 @@ chai.use(chaiAsPromised);
 const { assert } = chai;
 const errorMessage = 'Vulnerability detected';
 
+const cachedOutput = {};
+
+const getHost = function (url) {
+  if (url.startsWith('http:') || url.startsWith('https:')) {
+    return new URL(url).host;
+  }
+  return url;
+};
+
+const getSslyzeOutput = async function (hostname) {
+  const host = getHost(hostname);
+  if (!cachedOutput[host]) {
+    const sslyze = new Sslyze();
+    const results = await sslyze.startTheProcess(host);
+    const ciphersSupported = sslyze.listAcceptedCipherSuites();
+    const protocolSupported = ciphersSupported.reduce((protocols, cipherSuite) => {
+      if (protocols.includes(cipherSuite.type)) {
+        return protocols;
+      }
+      return protocols.concat(cipherSuite.type);
+    }, []);
+    cachedOutput[host] = {
+      sslyzeOutput: results,
+      ciphersSupported,
+      protocolSupported,
+    };
+    console.log(cachedOutput[host].sslyzeOutput.stdOut);
+  }
+  return cachedOutput[host];
+};
+
 module.exports = function () {
   this.When(/^the SSLyze command is run against the host$/, { timeout: env.longTimeout }, async function () {
-    const url = new URL(env.serverHostName);
-    const sslyze = new Sslyze();
-    this.sslyzeOutput = await sslyze.startTheProcess(url.host);
-    console.log(this.sslyzeOutput);
-    this.ciphersSupported = sslyze.listAcceptedCipherSuites();
-    this.protocolSupported = sslyze.listsupportedprotocols();
+    const output = await getSslyzeOutput(env.serverHostName);
+    Object.assign(this, output);
   });
 
   this.When(/^the SSLyze command is run against the "([^"]*)"$/, { timeout: env.longTimeout }, async function (host) {
-    const sslyze = new Sslyze();
-    this.sslyzeOutput = await sslyze.startTheProcess(host);
-    console.log(this.sslyzeOutput);
-    this.ciphersSupported = sslyze.listAcceptedCipherSuites();
-    this.protocolSupported = sslyze.listsupportedprotocols();
+    const output = await getSslyzeOutput(host);
+    Object.assign(this, output);
   });
 
   this.Then(/^the SSLyze output must contain the text "([^"]*)"$/, function (arg1) {
-    assert(this.sslyzeOutput.indexOf(arg1) !== -1, `${errorMessage}: SSLyze scan didnot contain any line matching - '${arg1}'`);
+    assert(this.sslyzeOutput.stdOut.indexOf(arg1) !== -1, `${errorMessage}: SSLyze scan did not contain any line matching - '${arg1}'`);
   });
 
   this.Then(/^the SSLyze output must contain a line that matches (.*)$/, async function (regex) {
-    assert(this.sslyzeOutput.search(regex) !== -1, `${errorMessage}: SSLyze scan didnot contain any line matching - '${regex}'`);
+    assert(this.sslyzeOutput.stdOut.search(regex) !== -1, `${errorMessage}: SSLyze scan did not contain any line matching - '${regex}'`);
   });
 
   this.Then(/^the minimum key size must be (.*) bits$/, function (arg1) {
@@ -50,28 +74,20 @@ module.exports = function () {
 
   this.Then(/^the following protocols must not be supported$/, async function (table) {
     assert(this.protocolSupported.length > 0, `${errorMessage}: The host does not support any valid protocols`);
-
     const data = table.hashes();
-    for (let i = 0; i < data.length; i += 1) {
-      this.protocolSupported.forEach((protocol) => {
-        if (protocol.name === data[i].protocol) {
-          assert.equal(protocol.type, 'Rejected', `${errorMessage}: ${protocol.name} should not be supported by host`);
-        }
-      });
-    }
+    const matches = data
+      .map(d => Sslyze.formatProtocol(d.protocol))
+      .filter(d => this.protocolSupported.includes(d));
+    assert.deepEqual(matches, [], `${errorMessage}: protocols must not be supported`);
   });
 
   this.Then(/^the following protocols must be supported$/, async function (table) {
     assert(this.protocolSupported.length > 0, `${errorMessage}: The host does not support any valid protocols`);
-
     const data = table.hashes();
-    for (let i = 0; i < data.length; i += 1) {
-      this.protocolSupported.forEach((protocol) => {
-        if (protocol.name === data[i].protocol) {
-          assert.equal(protocol.type, 'Supported', `${errorMessage}: ${protocol.name} should be supported by host`);
-        }
-      });
-    }
+    const matches = data
+      .map(d => Sslyze.formatProtocol(d.protocol))
+      .filter(p => !this.protocolSupported.includes(p));
+    assert.deepEqual(matches, [], `${errorMessage}: protocols must be supported`);
   });
 
   this.Then(/^any of the following ciphers must be supported$/, async function (table) {
@@ -93,11 +109,11 @@ module.exports = function () {
   });
 
   this.Then(/^the certificate has a matching host name$/, function () {
-    assert(this.sslyzeOutput.indexOf(`${env.serverHostName.replace(/^https?:\/\//,'')}`) !== -1, `${errorMessage}: None of Certificate has matching hostname - ${env.serverHostName.replace(/^https?:\/\//,'')}`);
+    assert(this.sslyzeOutput.stdOut.indexOf(`${env.serverHostName.replace(/^https?:\/\//, '')}`) !== -1, `${errorMessage}: None of Certificate has matching hostname - ${env.serverHostName.replace(/^https?:\/\//, '')}`);
   });
 
   this.Then(/^the certificate is in major root CA trust stores$/, function () {
-    assert(this.sslyzeOutput.search('OK - Certificate is trusted') !== -1, `${errorMessage}: Found Certificate that is not trusted`);
-    assert(this.sslyzeOutput.search('Certificate is not trusted') === -1, `${errorMessage}: Found Certificate that is not trusted`);
+    assert(this.sslyzeOutput.stdOut.search('OK - Certificate is trusted') !== -1, `${errorMessage}: Found Certificate that is not trusted`);
+    assert(this.sslyzeOutput.stdOut.search('Certificate is not trusted') === -1, `${errorMessage}: Found Certificate that is not trusted`);
   });
 };
--- integration/sslyze/service/index.js
+++ integration/sslyze/service/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-unresolved */
 const { exec } = require('child-process-promise');
+const { readFileSync } = require('fs');
 const Koa = require('koa');
 
 const app = new Koa();
@@ -11,10 +12,16 @@ const port = process.env.SVC_PORT || '8081';
 const svcTimeout = process.env.SSLYZE_SVC_TIMEOUT || '120000'; // 2 mins
 
 router.get('/sslyze/run/:host', async (ctx) => {
+  const hostName = ctx.params.host;
+  const fileName = `${hostName}.json`;
   try {
-    const result = await exec(`cd /usr/local/bin && ./sslyze --regular --http_headers ${ctx.params.host}`);
+    const result = await exec(`sslyze --regular --http_headers ${hostName} --json_out=${fileName}`);
     console.log(result.stdout); // eslint-disable-line
-    ctx.body = result.stdout;
+    ctx.headers = { 'Content-Type': 'application/json' };
+    ctx.body = {
+      stdOut: result.stdout,
+      json: JSON.parse(readFileSync(fileName, 'utf8')),
+    };
   } catch (err) {
     ctx.body = err;
   }
